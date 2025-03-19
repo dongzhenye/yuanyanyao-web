0c0226199e8d9bf845e83937a931da9b5f0816e6
Zhenye Dong <dongzhenye@gmail.com>
2025-03-19 12:10:57 +0800

    style: update all icons for better display

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,12 @@ export default function Document() {
   return (
     <Html lang="zh">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
-        <link rel="icon" type="image/png" href="/icon.png" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="原研药" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body className="antialiased">
         <Main />
